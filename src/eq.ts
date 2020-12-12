d7d0c0f488a59f6d58c8a4d79c5646ca0d670bfd
Dmitriy Khovrich <dkhovrich@readdle.com>
2020-12-12 16:04:06 +0200

    eq examples

---
+++ src/eq.ts
@@ -0,0 +1,53 @@
+import {contramap, eqDate, eqNumber, eqString, getMonoid, getStructEq} from "fp-ts/Eq";
+import {getEq} from "fp-ts/ReadonlyArray";
+import {fold} from "fp-ts/Monoid";
+
+type Avatar = {
+    initials: string;
+    url: string;
+}
+
+type User = {
+    userId: number;
+    name: string;
+    birthDate: Date;
+    avatar: Avatar;
+}
+
+// equals
+const eqAvatar = getStructEq<Avatar>({
+    initials: eqString,
+    url: eqString
+});
+
+const eqUser = getStructEq<User>({
+    userId: eqNumber,
+    name: eqString,
+    birthDate: eqDate,
+    avatar: eqAvatar
+});
+
+const r1 = eqUser.equals(
+    { userId: 1, name: "Peter Parker", birthDate: new Date("2000-01-01"), avatar: { initials: "PP", url: ""}},
+    { userId: 1, name: "Peter Parker", birthDate: new Date("2000-01-01"), avatar: { initials: "PP", url: ""}}
+    )
+console.log(r1);
+
+// contramap
+const eqUserById = contramap<number, User>(user => user.userId)(eqNumber);
+
+const r2 = eqUserById.equals(
+    { userId: 1, name: "Peter Parker", birthDate: new Date("2000-01-01"), avatar: { initials: "PP", url: ""}},
+    { userId: 1, name: "Gary Osborn", birthDate: new Date("2000-01-01"), avatar: { initials: "PP", url: ""}}
+);
+
+console.log(r2);
+
+// array equals
+const ar1 = [1, 2, 3];
+const ar2 = [1, 2, 3];
+
+const eqArrays = getEq(eqNumber);
+const r3 = eqArrays.equals(ar1, ar2);
+
+console.log(r3);
